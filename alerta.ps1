85bd0ef2caaaaa3572690f867f56762c523f7308
Matheus costa <139464891+matheusphb@users.noreply.github.com>
2025-11-20 11:02:40 -0300

    Update alerta.ps1

--- alerta.ps1
+++ alerta.ps1
@@ -1,4 +1,1 @@
-# Conteúdo do arquivo no GitHub (alerta.ps1)
-Write-Host "ATENÇÃO! ESTE É UM SIMULADO DE CONSCIENTIZAÇÃO." -ForegroundColor Red
-Write-Host "Voce clicou em um arquivo LNK malicioso." -ForegroundColor Yellow
-Read-Host "Pressione ENTER para sair"
+
